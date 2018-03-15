6cebcfc2a395e11f46929949fd0da86580634254
3dw1nM0535 <workockmoses@gmail.com>
2018-03-15 19:24:06 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Nightlife Coordination App
+
+Find and add yourself in a local bar at your area.
+
+I used [Yelp API](https://www.yelp.com/fusion) services to find local businesses depending on your location.
+
+
+
+This project was bootstraped by [Webpack](https://webpack.js.org)
---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+// ES6 code transpiler
+
+var path = require("path");
+var webpack = require("webpack");
+var autoprefixer = require("autoprefixer");
+
+module.exports = {
+  mode: "development",
+  devtool: "eval",
+  entry: [
+    "webpack-hot-middleware/client",
+    path.join(__dirname, "/client/index.js"),
+  ],
+  output: {
+    path: path.join(__dirname, "build"),
+    filename: "bundle.js",
+    publicPath: "/build/",
+  },
+  plugins: [
+    new webpack.optimize.OccurrenceOrderPlugin(),
+    new webpack.NoEmitOnErrorsPlugin(),
+  ],
+  module: {
+    rules: [
+      { test: /\.(woff2?|svg|jpe?g|png|gif|ico|eot|ttf)$/, loader: "url-loader?limit=10000" },
+      {
+        // JS
+        test: /\.js$/,
+        loaders: ["babel-loader?" + JSON.stringify({ cacheDirectory: true }), ],
+        include: path.join(__dirname, "/client"),
+      },
+      {
+        // CSS
+        test: /\.css$/,
+        use: [
+          "style-loader",
+          { loader: "css-loader", options: { importLoaders: 1 } },
+
+          // Configure for CSS imports to work
+          { loader: "postcss-loader", options: { ident: "postcss", plugins: () => [
+            require("postcss-flexbugs-fixes"),
+            autoprefixer({
+              browsers: [
+                ">1%",
+                "last 4 versions",
+                "Firefox ESR",
+                "not ie < 9",
+              ],
+              flexbox: "no-2009"
+            })
+          ]}},
+        ],
+      }
+    ],
+  },
+  resolve: {
+    extensions: [".js", ".jsx", ".json"],
+  }
+}
