c693bf3230341e58ce68f56728647b1efd4a7506
3dw1nM0535 <workockmoses@gmail.com>
2018-03-19 22:42:24 +0300

    user signup API route handler

--- server/routes/user/auth.js
+++ server/routes/user/auth.js
@@ -1,3 +1,22 @@
-//////////////////////////////////////////////
-//User authentication request route handler //
-//////////////////////////////////////////////
+///////////////////////////////////////////////
+//User one time signup request route handler //
+///////////////////////////////////////////////
+import express from "express";
+import User from "../../model/user";
+
+import parseErrors from "../../utils/parseError";
+
+const router = express.Router();
+
+router.post("/", (req, res) => {
+  const { email, password } = req.body.data;
+  const user = new User({
+    email,
+  });
+  user.setPassword(password);
+  user.save().then((userRecord) => {
+    res.json({ user: userRecord });
+  }).catch(err => res.status(400).json({ errors: parseErrors(err.errors) }));
+});
+
+export default router;
