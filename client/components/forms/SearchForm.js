d90b26ea9874c65098031f210c165dffea7b0675
3dw1nM0535 <workockmoses@gmail.com>
2018-03-20 11:41:23 +0300

    control request to server by disabling input control and button while server is processing a request

--- client/components/forms/SearchForm.js
+++ client/components/forms/SearchForm.js
@@ -46,8 +46,8 @@ class SearchForm extends React.Component {
       <div>
         <Form size="tiny" onSubmit={this.onSubmit}>
           <Form.Group widths="equal">
-            <Form.Input fluid name="location" value={data.location} onChange={this.onChange} placeholder="Enter location to search" error={!!errors.location} />
-            <Button primary size="tiny">Search</Button>
+            <Form.Input disabled={isLoading} fluid name="location" value={data.location} onChange={this.onChange} placeholder="Enter location to search" error={!!errors.location} />
+            <Button disabled={isLoading} primary size="tiny">Search</Button>
           </Form.Group>
           { errors.location && <InlineError text={errors.location} /> }
         </Form>
