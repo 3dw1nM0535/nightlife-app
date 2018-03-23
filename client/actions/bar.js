0e101f2ae7d28f5abf2fc96ea1f79bbb30a8569c
3dw1nM0535 <workockmoses@gmail.com>
2018-03-23 21:09:23 +0300

    bind user id for record keeping

--- client/actions/bar.js
+++ client/actions/bar.js
@@ -5,8 +5,16 @@ import axios from "axios";
 
 import { locateBars } from "../actions/actionCreators";
 
+////////////////////
+//Find bar action //
+////////////////////
 export const findBars = data => dispatch =>
   axios.post("/api/bars/find", { data }).then(res => res.data.bars)
     .then((bars) => {
       dispatch(locateBars(bars));
     });
+/////////////////////////////////
+//Update bar to indicate going //
+/////////////////////////////////
+export const indicateGoing = _id => dispatch =>
+  axios.post("/api/bar/update", { _id }).then(res => res.data.bars);
--- client/components/cta/Item.js
+++ client/components/cta/Item.js
@@ -1,13 +1,21 @@
 import React from "react";
 import { connect } from "react-redux";
 import PropTypes from "prop-types";
-import { Item, Button } from "semantic-ui-react";
+import { Item, Button, Label } from "semantic-ui-react";
+
+import { indicateGoing } from "../../actions/bar";
 
 class Content extends React.Component {
   constructor(props) {
     super(props);
     this.state = {};
   }
+
+  handleClick = (id) => {
+    const _id = id;
+    this.props.indicateGoing(_id);
+  }
+
   render() {
     const { bars, isAuthenticated } = this.props;
 
@@ -15,13 +23,14 @@ class Content extends React.Component {
       <Item.Group divided relaxed>
         { !bars.businesses ? null : (
           bars.businesses.map(item =>
-          <Item>
+          <Item key={item._id}>
             <Item.Image size="small" src={item.image_url} />
             <Item.Content verticalAlign="middle">
               <Item.Header>{item.name}</Item.Header>
               <Item.Description>{item.id}</Item.Description>
               <Item.Extra>
-                <Button size="tiny" color="blue" floated="right">Going?</Button>
+                <Button onClick={this.handleClick.bind(this, item._id)} size="tiny" color="blue">Going?</Button>
+                <Label circular color="grey">{parseInt(item.going_count, 10)}</Label>
               </Item.Extra>
             </Item.Content>
           </Item>)
@@ -44,6 +53,7 @@ Content.propTypes = {
     _v: PropTypes.number,
     businesses: PropTypes.arrayOf(PropTypes.object),
   }).isRequired,
+  indicateGoing: PropTypes.func.isRequired,
 };
 
-export default connect(mapStateToProps)(Content);
+export default connect(mapStateToProps, { indicateGoing })(Content);
